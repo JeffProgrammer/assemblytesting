680e8ae48bc4c6b96b738b0a9602d74f445c313b
Jeff Hutchinson <programmerjeff95@gmail.com>
2016-04-01 14:07:06 -0400

    add two numbers

--- main.cpp
+++ main.cpp
@@ -9,17 +9,32 @@ void helloASM() {
 	{
 		; Push hello world pointer into eax register
 		mov eax, offset helloWorld
-			push eax
-			call printf; invoke prinf
-			pop ebx
+		push eax
+		call printf; invoke prinf
+		pop ebx
 	}
 #else
 	printf("%s", helloWorld);
 #endif
 }
 
+int add2Numbers(int a, int b) {
+#ifdef _WIN32
+	__asm {
+		mov eax, a
+		mov ebx, b
+		add eax, ebx
+	}
+#else
+	return a + b;
+#endif
+}
+
 int main(int argc, const char **argv) {
 	helloASM();
+
+	int x = add2Numbers(4, 9);
+	printf("x is: %d\n", x);
 
 #ifdef _WIN32
 	system("pause");
